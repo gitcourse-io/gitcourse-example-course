7bfcff14bcc700e05633235a86b2cbca4ab31997
guodong <gd@tongji.edu.cn>
2019-06-15 02:14:05 +0800

    update

--- bg.sh
+++ bg.sh
@@ -1,1 +1,1 @@
-jupyter notebook  –NotebookApp.tornado_settings='{"headers":{"Content-Security-Policy":"frame-ancestors self http://localhost:3000 http://localhost:3001 http://kfcoding.com;" }}'
+jupyter notebook –NotebookApp.tornado_settings='{"headers":{"Content-Security-Policy":"frame-ancestors self http://localhost:3000 http://localhost:3001 http://kfcoding.com;" }}'
